7ca4ec0e828cb2f5dc7d129a58c999d764f44acf
faric91 <a.v.krypsky@gmail.com>
2017-07-03 20:55:47 +0300

    03.07.2017_20.55

---
+++ st-project9/assets/js/refbooks.goods.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".btn-open-modal-goods-prices").click(function(){
+        $("#modal-goods-prices").modal("show");
+    });
+    $("#btn-open-modal-add-new-goods").click(function(){
+        $("#modal-add-new-goods").modal("show");
+    })
+})
